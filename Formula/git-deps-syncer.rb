878b55caea517010f59b44b5de819ec03ecf54d9
Zachi Nachshon <zachi.nachshon@gmail.com>
2022-06-22 21:38:42 +0300

    [git-deps-syncer] Upgrade to version 0.3.0

--- Formula/git-deps-syncer.rb
+++ Formula/git-deps-syncer.rb
@@ -4,9 +4,9 @@
 class GitDepsSyncer < Formula
   desc "Sync git repositories as external source dependencies"
   homepage "https://ZachiNachshon.github.io/git-deps-syncer/"
-  version "0.2.0"
-  url "https://github.com/ZachiNachshon/git-deps-syncer/releases/download/v0.2.0/git-deps-syncer.sh"
-  sha256 "ce9e0c13c8307eb882836adb55ddf1ce025615f6fea72f4517d08abdc3cebefb"
+  version "0.3.0"
+  url "https://github.com/ZachiNachshon/git-deps-syncer/releases/download/v0.3.0/git-deps-syncer.sh"
+  sha256 "1a3ce17e9190f9bd83d9ba98180a39c53f40410cd5495d417d901e7c90690e3b"
   license "MIT"
 
   def install
