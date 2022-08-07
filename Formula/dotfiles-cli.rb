82a20a4b987e6f05814d9d0f080b71e920998d5b
Zachi Nachshon <zachi.nachshon@gmail.com>
2022-08-07 08:05:51 +0300

    [dotfiles-cli] Install all extracted content and create a relative symlink to bin folder

--- Formula/dotfiles-cli.rb
+++ Formula/dotfiles-cli.rb
@@ -9,13 +9,14 @@ class DotfilesCli < Formula
   sha256 "0ce66da6a2aee37e11b2a7cf9292fe0b446195ba0febc7c29da541e4e4cc8ed4"
   license "MIT"
 
-  def install
-    # Add the dotfiles script header to RC file (zsh, bashrc, bash_profile)
-    bin.install 'HOMEBREW_INSTALL="True" install.sh'
-    bin.install_symlink "dotfiles.sh" => "dotfiles"
-  end
-
   depends_on "git"
+
+  def install
+    # Add extracted files to the Homebrew install directory
+    libexec.install Dir["*"]
+    # Add a relative symlink from Homebrew libexec to bin folder
+    bin.install_symlink libexec/"dotfiles.sh" => "dotfiles"
+  end
   
   test do
     system "#{bin}/dotfiles version"
