31fc16b70cd22ac6dc7a5612a8f8937c226ea5c9
Zachi Nachshon <zachi.nachshon@gmail.com>
2022-09-13 11:53:06 +0300

    [git-deps-syncer] Update to version 0.6.0

--- Formula/git-deps-syncer.rb
+++ Formula/git-deps-syncer.rb
@@ -4,9 +4,9 @@
 class GitDepsSyncer < Formula
   desc "Sync git repositories as external source dependencies"
   homepage "https://ZachiNachshon.github.io/git-deps-syncer/"
-  version "0.5.0"
-  url "https://github.com/ZachiNachshon/git-deps-syncer/releases/download/v0.5.0/git-deps-syncer.tar.gz"
-  sha256 "3482e32ec5ac467788fd4f81832bb4d6782f508bebcf2dcc3896a12a79b2add5"
+  version "0.6.0"
+  url "https://github.com/ZachiNachshon/git-deps-syncer/releases/download/v0.6.0/git-deps-syncer.tar.gz"
+  sha256 "e478b648fac03864fef85e2db1c197c8db81f0d6d34f3584955fc4a56bb30c41"
   license "MIT"
 
   depends_on "git"
